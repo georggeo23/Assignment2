9499ca92e3e7b1be4dc3dbc18d0b0c1686f130e5
georggeo23 <georgechris2003@gmail.com>
2024-02-17 20:49:24 -0500

    Restconfig--update

--- src/main/java/sheridan/georggeo/assignment2/config/RestConfig.java
+++ src/main/java/sheridan/georggeo/assignment2/config/RestConfig.java
@@ -1,8 +1,13 @@
 package sheridan.georggeo.assignment2.config;
 
+import sheridan.georggeo.assignment2.data.jpa.Art;
+import sheridan.georggeo.assignment2.data.jpa.ArtOwner;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RestConfig implements  RepositoryRestConfigurer{
         @Override
         public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
